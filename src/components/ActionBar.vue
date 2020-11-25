c74b14d0dfeb31b1096cd0147160cec9d44f7653
Anthony Dionise <anthonydionise@Anthonys-MacBook-Pro.local>
2020-11-24 22:40:21 -0500

    basic page

---
+++ src/components/ActionBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app-bar app dark class="actionBar">
+        <div class="d-flex align-center">
+            <h1>Thots and Shots</h1>
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+            href="https://github.com/vuetifyjs/vuetify/releases/latest"
+            target="_blank"
+            text
+        >
+            <!-- <span class="mr-2">Latest Release</span>
+            <v-icon>mdi-open-in-new</v-icon> -->
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'ActionBar',
+    data: () => ({
+    
+    })
+}
+</script>
